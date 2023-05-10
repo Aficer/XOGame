01382b6212d3c5c8ffe57b00c44e9821157d739a
Aficer <Aficer89@gmail.com>
2023-05-10 18:51:24 +0300

    XOGame
    Test commit

---
+++ XOGame/XOGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  XOGameApp.swift
+//  XOGame
+//
+//  Created by Вадим Эфис on 10.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct XOGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
